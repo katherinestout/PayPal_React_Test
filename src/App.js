f7b5005d1da0540deee5dec154ca255f7e72019c
katherinestout <kjs14@hood.edu>
2019-02-08 10:47:34 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PayPalButton from './PayPalButton';
 import './App.css';
+
+//require('dotenv').configure();
+
+/*
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'ARTyH8uojFhbF0OMuhYnsGtjwr6YOBuVyR1aShZmdv3zWRWIpO3UZMi6UcYvId-jFSFcXNB_Dipqg9Ba',
+  'client_secret': 'EDvuzF7pbswLpmhGs2W6MtZrBNm5AOmwJT34fkByoeeUkNYkqcHpyay6TW7AFWSCSV-xEUymTI2A9OYW'
+});
+*/
+
+require('dotenv').config(); 
+
+const CLIENT = {
+  sandbox: process.env.REACT_APP_client_id,
+  production: 'xxxXXX',
+};
+
+const ENV = process.env.NODE_ENV === 'production'
+  ? 'production'
+  : 'sandbox';
+
 
 class App extends Component {
   render() {
+
+   
+
+  const onSuccess = (payment) =>
+  console.log('Successful payment!', payment);
+
+  const onError = (error) =>
+    console.log('Erroneous payment OR failed to load script!', error);
+
+  const onCancel = (data) =>
+    console.log('Cancelled payment!', data);
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <PayPalButton
+          client={CLIENT}
+          env={ENV}
+          commit={true}
+          currency={'USD'}
+          total={100}
+          onSuccess={onSuccess}
+          onError={onError}
+          onCancel={onCancel}
+        />
       </div>
     );
   }
